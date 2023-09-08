8327ec862ca552e5d64feb8153dedb8f86dac82b
mazzwell <137079218+mazzwell@users.noreply.github.com>
2023-09-08 19:06:10 +0600

    Update contractAddresses.ts

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0xFE029F37c2201f95D1a5756d56D3100fe230Dd21";
+  "0x0C7921b602eC1c39D29e2c312f35D55141af3E1A";
 export const tokenContractAddress =
-  "0x851dC1981aEb9fCF446055Ac7EC8670f6a9e9C72";
+  "0x00ff0c7CA0C36d829967408599A0E1D3D2D6EeF7";
 export const stakingContractAddress =
-  "0x4bCbBC25Cc2Cf2b0Ed4dC246Fe74F3a5b38c4849";
+  "0xed676fd05c4337494a0851D9619cA5Efa5a42Fb2";
